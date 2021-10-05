bdad594c007204dc18d1e3e09929fc1f6958786d
nokza007 <6406021410039@fitm.kmutnb.ac.th>
2021-10-05 14:39:20 +0700

    cpplab6

---
+++ 6_2_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Sum = 0;
+
+    Sum = Sum + 1;
+    Sum = Sum + 2;
+    Sum = Sum + 3;
+    Sum = Sum + 4;
+    Sum = Sum + 5;
+    Sum = Sum + 6;
+    Sum = Sum + 7;
+    Sum = Sum + 8;
+    Sum = Sum + 9;
+    Sum = Sum + 10;
+
+    cout << "Total of array elements : " << Sum << endl;
+    return(0);
+}
